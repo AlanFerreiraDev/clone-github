1d8ffdf30cd3e65968adc8b736aa07bf7f0ced50
Alan Bruno Ferreira da Silva <alanjjr83@gmail.com>
2022-02-21 18:45:34 -0300

    start: first commit

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export const Profile = () => {
+  return (
+    <Container>
+      <h1>Profile</h1>
+    </Container>
+  );
+};
---
+++ src/pages/Repo/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export const Repo = () => {
+  return (
+    <Container>
+      <h1>Repo</h1>
+    </Container>
+  );
+};
