62cced1c60c0b951343dbcade3f55468c984c2a2
Alan Bruno Ferreira da Silva <alanjjr83@gmail.com>
2022-02-22 00:05:29 -0300

    add: New Features Profile Page

---
+++ src/components/RandomCalendar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const RandomCalendar = () => {
+  return (
+    <Container>
+      <h1>RandomCalendar</h1>
+    </Container>
+  );
+};
+
+export default RandomCalendar;
